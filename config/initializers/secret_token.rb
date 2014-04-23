6d00d9d257ff8e1284117ddbf93a50bd24eef0a5
unknown <glevin@A20909.bcis.bates.edu>
2014-04-23 10:49:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintrestforkittens::Application.config.secret_key_base = 'ab47d8d439b3469a0a70afb93b3666fdab9f1806d04b95548607c506c323474c633514960f1cff2f8b79a951f8b2062b7a9de706a9ebe203c25a2717de6ab2a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintrestforkittens::Application.config.session_store :cookie_store, key: '_pintrestforkittens_session'
