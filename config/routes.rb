361f94ce5b12d5496abb17f30bf266772aae62a9
thecrazychimpz <glevin@A20909.bcis.bates.edu>
2014-05-01 11:12:22 -0400

    j

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pintrestforkittens::Application.routes.draw do
   #get "page/index"
 
+  get '/about'=>"page#about"
   root "page#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
