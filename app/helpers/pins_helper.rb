31492493fa539960f577f0dc1260409563f3b8a8
thecrazychimpz <glevin@A20909.bcis.bates.edu>
2014-05-14 14:55:26 -0400

    ohhh

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -1,2 +1,9 @@
 module PinsHelper
+  def descriptions_error
+    if @pin.errors[:descriptions].size > 0
+      return "has-error"
+    end
+
+    return ""
+  end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
 	validates :descriptions, presence: true
+	validates :descriptions, length: {minimum: 5}
+	belongs_to :user
 end
